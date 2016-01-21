3cdd9a68b91afe511d7644dcc2d681e39699b1e2
Douglas Selph <doug@tipsolutions.com>
2016-01-20 23:09:40 -0600

    started new blank fell project

---
+++ Gdx/fell/android/src/com/dugsolutions/felldungeon/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dugsolutions.felldungeon;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dugsolutions.felldungeon.FellDungeon;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FellDungeon(), config);
+	}
+}
---
+++ Gdx/fell/desktop/src/com/dugsolutions/felldungeon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dugsolutions.felldungeon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dugsolutions.felldungeon.FellDungeon;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FellDungeon(), config);
+	}
+}
