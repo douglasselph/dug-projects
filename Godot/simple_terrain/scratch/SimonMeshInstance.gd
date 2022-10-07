1e2761baaaae194affb42e7db796cd955b2150dc
Douglas Selph <douglas.selph@kajabi.com>
2022-10-07 10:21:07 -0500

    Second Try
    Completely rebuild the base with tutorial example, hterrain example, and core of own integration in scratch

--- Godot/simple_terrain/scratch/SimonMeshInstance.gd
+++
@@ -1,8 +0,0 @@
-extends MeshInstance
-
-func _ready():
-	mesh.material.set_shader_param("height_scale", 0.5)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
